70f732ec2467e1c6a3f796974107d84e65626c79
秀和 <hidekazu5200@gmail.com>
2018-08-16 11:26:56 +0900

    jsを読み込めるようにした。

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -23,5 +23,6 @@
   <script type="text/javascript" src="{{asset('js/jquery-3.1.1.js')}}"></script>
   <script type="text/javascript" src="{{asset('js/jquery-migrate-1.4.1.js')}}"></script>
   <script type="text/javascript" src="{{asset('js/bootstrap.js')}}"></script>
+  @yield('js')
 </body>
 </html>
