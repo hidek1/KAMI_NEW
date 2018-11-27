314cd14061247e3610217a50bf8913bf871ebc0d
Yuta-Sudo <yuta.sudo.gis@gmail.com>
2018-11-28 01:41:58 +0900

    カラム名称の変更

--- database/migrations/2018_11_27_083223_create_event_relations_table.php
+++ database/migrations/2018_11_27_083223_create_event_relations_table.php
@@ -18,8 +18,8 @@ class CreateEventRelationsTable extends Migration
             $table->integer('user_id')->unsigned();
             $table->integer('event_id')->unsigned();
             $table->tinyInteger('relation_fl')->default(0);
-            $table->dateTime('login_created')->default(DB::raw('CURRENT_TIMESTAMP'));
-            $table->dateTime('login_modified')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->dateTime('relation_created')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->dateTime('relation_modified')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
             $table->foreign('user_id')->references('user_id')->on('users'); //外部キー参照
             $table->foreign('event_id')->references('event_id')->on('events'); //外部キー参照
         });
